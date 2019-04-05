45ca6665d3eb708536169ce465f6b42a5d5ca568
nzbenji <benlittle53@gmail.com>
2019-04-05 13:01:42 +1300

    Added navigation control and ability to move map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactMapGL from 'react-map-gl'
+import React, {useState} from 'react'
+import ReactMapGL, {NavigationControl} from 'react-map-gl'
 
-const viewport = {
+const INITIAL_VIEWPORT = {
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 13
 }
 
 const Map = () => {
+    const [viewport, updateViewport] = useState(INITIAL_VIEWPORT)
+
     const API_KEY = "pk.eyJ1Ijoia2FsYWR6ZSIsImEiOiJjanRub28wcDQzdW5qNGJtdXN3YmJ1MnNhIn0.4R0arj8vtdr_cpcDdB5Agw"
     return (
         <div>
@@ -16,9 +18,13 @@ const Map = () => {
                 height="calc(100vh - 64px)" /*Height of page minus nav bar height*/
                 mapStyle="mapbox://styles/mapbox/streets-v9"
                 mapboxApiAccessToken={API_KEY}
-                {...viewport}
-            >
-
+                onViewportChange={newViewport => updateViewport(newViewport)}
+                {...viewport} 
+            > 
+                {/*Navigation Control*/}
+                <div>
+                    <NavigationControl onViewportChange={newViewport => updateViewport(newViewport)} />
+                </div>
             </ReactMapGL>
         </div>
     )
