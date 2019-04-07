e4697b132b351eea2d9ce6db5399541c2a2b629f
nzbenji <benlittle53@gmail.com>
2019-04-08 11:49:06 +1200

    No content styling

--- client/src/components/Pin/NoContent.js
+++ client/src/components/Pin/NoContent.js
@@ -1,10 +1,34 @@
 import React from "react"
+import { faCompass } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    min-height: 50vh;
+
+    h2 {
+        text-align: center;
+        letter-spacing: 1.8px;
+    }
+`
+
 
 const NoContent = () => {
     return (
-        <div>
-            No Content
-        </div>
+        <Wrapper>
+            <FontAwesomeIcon 
+                style={{justifyContent: 'center'}}
+                icon={faCompass} 
+                size="6x"
+                color="orange"
+            />
+            <h2>Click on map to add a pin</h2>
+        </Wrapper>
     )
 }
 
