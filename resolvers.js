3fc7731471f61a72f783606409bb8ea914e72f04
nzbenji <benlittle53@gmail.com>
2019-04-08 15:11:47 +1200

    graphql mutations for creating pins

--- resolvers.js
+++ resolvers.js
@@ -1,11 +1,5 @@
 const {AuthenticationError} = require('apollo-server')
-
-const user = {
-    _id: '1', 
-    name: 'Ben',
-    email: 'ben@mail.com',
-    picture: 'url.com'
-}
+const Pin = require('./models/Pin')
 
 const authenticated = next => (root, args, context, info) => {
     if(!context.currentUser) {
@@ -17,5 +11,16 @@ const authenticated = next => (root, args, context, info) => {
 module.exports = {
     Query: {
         me: authenticated((root, args, context) => context.currentUser)
+    },
+    Mutation: {
+        //Create a pin with authenticated user as the author of pin
+        createPin: authenticated(async (root, args, ctx) => {
+            const newPin = await new Pin({
+                ...args.input,
+                author: ctx.currentUser._id
+            }).save()
+            const addPin = await Pin.populate(newPin, 'author')
+            return addPin
+        })
     }
 }
