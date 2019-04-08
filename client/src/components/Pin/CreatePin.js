a144b7690fd19cea160a930d256e979c5abe5c93
nzbenji <benlittle53@gmail.com>
2019-04-08 14:22:06 +1200

    Added and clearing state on button click

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,10 +1,31 @@
-import React from "react"
+import React, {useState, useContext} from "react"
 import { faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import CreatePinWrapper from '../styles/createPin'
 import { Button, Form, TextArea, Input } from 'semantic-ui-react'
 
+import Context from '../../context'
+
 const CreatePin = () => {
+    const {dispatch} = useContext(Context)
+    const [title, setTitle] = useState("")
+    const [image, setImage] = useState("")
+    const [content, setContent] = useState("")
+
+    const handleSubmit = event => {
+        event.preventDefault()
+
+        console.log({title, image, content})
+    }
+
+    const handleDelete = () => {
+        setTitle("")
+        setImage("")
+        setContent("")
+
+        dispatch({ type: 'DELETE_DRAFT'})
+    }
+
     return (
         <CreatePinWrapper>
             <div>
@@ -17,23 +38,36 @@ const CreatePin = () => {
             </div>
             
             <div>
-                <Input placeholder='Title' style={{marginBottom: '20px'}}/>
+                <Input 
+                    placeholder='Title' 
+                    style={{marginBottom: '20px'}}
+                    onChange={event => setTitle(event.target.value)}
+                />
 
                 <label htmlFor="image">
                 <input 
                     accept="image/*"
                     id="image"
                     type="file"
+                    onChange={event => setImage(event.target.files[0])}
                 />
             </label>
             </div>
             
             <Form>
-                <TextArea placeholder='Tell us more' style={{ minHeight: 100, marginBottom: '20px' }} />
+                <TextArea 
+                    placeholder='Tell us more' 
+                    style={{ minHeight: 100, marginBottom: '20px' }} 
+                    onChange={event => setContent(event.target.value)}
+                />
             </Form>
             <div>
-                <Button negative style={{marginRight: '10px'}}>Discard</Button>
-                <Button positive>Submit</Button>
+                <Button negative style={{marginRight: '10px'}} onClick={handleDelete}>Discard</Button>
+                <Button 
+                    positive
+                    disabled={!title.trim() || !content.trim() || !image}
+                    onClick={handleSubmit}
+                >Submit</Button>
             </div>
 
         </CreatePinWrapper>
